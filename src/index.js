4c0d599937dd9f427baf7f0fe4df0d14d8e2ab0e
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-12 14:32:41 -0500

    updating re

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const $ = require('jquery');
 console.log('sayHello');
+document.write('testingForVic');
+document.write(' How else should we test this?');
+
+// addingHTML
+
+$('body').html('<h1>Hello Vic</h1><h2> hello</h2>');
