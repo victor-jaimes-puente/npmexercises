c3597e3d45aee9714f45139ec5b8b55ed2d145bf
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-12 16:28:19 -0500

    updating tree

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const $ = require('jquery');
+const testRequire = require('./public/say-hello.js');
 console.log('sayHello');
 document.write('testingForVic');
 document.write(' How else should we test this?');
 
 // addingHTML
 
-$('body').html('<h1>Hello Vic</h1><h2> hello</h2>');
+$('body').append('<h1>Hello Vic</h1><h2> hello</h2>');
+$('h1').css('background', 'red');
+$('h2').css('background', 'red');
+
+testRequire.sayHello;
